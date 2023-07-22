3c6ece6d0e191ab877a7f721edd2974af0ba26b8
Raphaël Mitas <raphael.mitas@web.de>
2023-07-22 09:52:37 +0200

    added main function

--- tft-data-prep/prep_augments.py
+++ tft-data-prep/prep_augments.py
@@ -1,25 +1,41 @@
-
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 
+def main():
+    load_dotenv()
 
-load_dotenv()
+    mongo_pw = os.environ['MONGO_DB_PASSWORD']
 
-mongo_pw = os.environ['MONGO_DB_PASSWORD']
+    uri = f"mongodb+srv://admin:{mongo_pw}@ruffysfeuerstelle.jw6klif.mongodb.net/?retryWrites=true&w=majority"
 
-uri = f"mongodb+srv://admin:{mongo_pw}@ruffysfeuerstelle.jw6klif.mongodb.net/?retryWrites=true&w=majority"
+    print("Connecting to MongoDB...")
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    print("Connected!")
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
 
+    print("Creating game_versions collection...")
+    result = client['ruffys-feuerstelle']['tftmatches'].aggregate([
+        {
+            '$group': {
+                '_id': '$info.game_version', 
+                'count': {
+                    '$sum': 1
+                }, 
+                'earliest_date': {
+                    '$min': '$info.game_datetime'
+                }, 
+                'latest_date': {
+                    '$max': '$info.game_datetime'
+                }
+            }
+        }, {
+            '$out': 'game_versions'
+        }
+    ])
+    print("Done!")
 
-
-print(client.list_database_names())
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    main()
