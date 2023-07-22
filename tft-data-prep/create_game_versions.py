a63145f4cb24be0ece49849b3a9c13e8009b6190
Raphaël Mitas <raphael.mitas@web.de>
2023-07-22 11:52:22 +0200

    sure but don't need data-prep anymore

--- tft-data-prep/create_game_versions.py
+++ tft-data-prep/create_game_versions.py
@@ -19,23 +19,28 @@ def main():
 
     print("Creating game_versions collection...")
     result = client['ruffys-feuerstelle']['tftmatches'].aggregate([
-        {
-            '$group': {
-                '_id': '$info.game_version', 
-                'count': {
-                    '$sum': 1
-                }, 
-                'earliest_date': {
-                    '$min': '$info.game_datetime'
-                }, 
-                'latest_date': {
-                    '$max': '$info.game_datetime'
-                }
+    {
+        '$group': {
+            '_id': '$info.game_version', 
+            'tft_set_core_name': {
+                '$first': '$info.tft_set_core_name'
+            }, 
+            'tft_set_number': {
+                '$first': '$info.tft_set_number'
+            }, 
+            'count': {
+                '$sum': 1
+            }, 
+            'earliest_date': {
+                '$min': '$info.game_datetime'
+            }, 
+            'latest_date': {
+                '$max': '$info.game_datetime'
             }
-        }, {
-            '$out': 'game_versions'
         }
-    ])
+    }, {
+        '$out': 'game_versions'
+    }])
     print("Done!")
 
 if __name__ == "__main__":
--- tft-data-prep/prep_augments.py
+++ tft-data-prep/prep_augments.py
@@ -3,6 +3,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 
+
 def main():
     load_dotenv()
 
@@ -14,6 +15,71 @@ def main():
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
     print("Connected!")
+
+    game_versions = client.get_database(
+        'ruffys-feuerstelle').get_collection('game_versions').find()
+
+    for game_version in game_versions:
+        print(game_version)
+        tft_matches = client.get_database('ruffys-feuerstelle').get_collection(
+            'tftmatches').find({'info.game_version': game_version['_id']})
+
+        result = client['ruffys-feuerstelle']['tftmatches'].aggregate([
+            {
+                '$match': {
+                    'info.game_version': 'Version 13.13.518.0539 (Jun 28 2023/03:11:33) [PUBLIC] <Releases/13.13>'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$info.participants'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$info.participants.augments',
+                    'includeArrayIndex': 'augment_at_stage'
+                }
+            }, {
+                '$addFields': {
+                    'augment_at_stage': {
+                        '$add': [
+                            '$augment_at_stage', 2
+                        ]
+                    }
+                }
+            }, {
+                '$group': {
+                    '_id': {
+                        'augment': '$info.participants.augments',
+                        'augment_at_stage': '$augment_at_stage',
+                        'game_version': '$info.game_version'
+                    },
+                    'average_placement_at_stage': {
+                        '$avg': '$info.participants.placement'
+                    },
+                    'total_games': {
+                        '$sum': 1
+                    },
+                    'wins': {
+                        '$sum': {
+                            '$cond': [
+                                {
+                                    '$eq': [
+                                        '$info.participants.placement', 1
+                                    ]
+                                }, 1, 0
+                            ]
+                        }
+                    }
+                }
+            }, {
+                '$sort': {
+                    '_id.augment_at_stage': -1
+                }
+            }, {
+                '$out': 'augments_at_stage'
+            }
+        ])
+
 
 if __name__ == "__main__":
     main()
