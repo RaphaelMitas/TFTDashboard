39daaa8cdf6d62ad2d59b6cf24fe4a4c59bffa9f
Raphael Mitas <raphael.mitas@web.de>
2023-07-24 12:55:00 +0200

    created deploy for mongodb in digital ocean

--- tft-data-prep/prep_augments.py
+++ tft-data-prep/prep_augments.py
@@ -1,84 +1,119 @@
-from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 
 
 def main():
-    load_dotenv()
 
-    mongo_pw = os.environ['MONGO_DB_PASSWORD']
+    DO_MONGO_DB_CONNECTION_STRING = os.environ['DO_MONGO_DB_CONNECTION_STRING']
+    DO_MONGO_DB_PASSWORD = os.environ['DO_MONGO_DB_PASSWORD']
+    DO_MONGO_DB_USER = os.environ['DO_MONGO_DB_USER']
 
-    uri = f"mongodb+srv://admin:{mongo_pw}@ruffysfeuerstelle.jw6klif.mongodb.net/?retryWrites=true&w=majority"
+    uri = f"mongodb+srv://{DO_MONGO_DB_USER}:{DO_MONGO_DB_PASSWORD}@{DO_MONGO_DB_CONNECTION_STRING}"
 
     print("Connecting to MongoDB...")
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
     print("Connected!")
 
-    game_versions = client.get_database(
-        'ruffys-feuerstelle').get_collection('game_versions').find()
-
-    for game_version in game_versions:
-        print(game_version)
-        tft_matches = client.get_database('ruffys-feuerstelle').get_collection(
-            'tftmatches').find({'info.game_version': game_version['_id']})
-
-        result = client['ruffys-feuerstelle']['tftmatches'].aggregate([
-            {
-                '$match': {
-                    'info.game_version': 'Version 13.13.518.0539 (Jun 28 2023/03:11:33) [PUBLIC] <Releases/13.13>'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$info.participants'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$info.participants.augments',
-                    'includeArrayIndex': 'augment_at_stage'
-                }
-            }, {
-                '$addFields': {
-                    'augment_at_stage': {
-                        '$add': [
-                            '$augment_at_stage', 2
+    print("Preparing augments...")
+    result = client['ruffys-feuerstelle']['augments_at_stage_view'].aggregate([
+        {
+            '$group': {
+                'average_placement_at_stage_3': {
+                    '$avg': {
+                        '$cond': [
+                            {
+                                '$eq': [
+                                    '$_id.augment_at_stage', 3
+                                ]
+                            }, '$average_placement_at_stage', None
+                        ]
+                    }
+                },
+                'average_placement_at_stage_4': {
+                    '$avg': {
+                        '$cond': [
+                            {
+                                '$eq': [
+                                    '$_id.augment_at_stage', 4
+                                ]
+                            }, '$average_placement_at_stage', None
+                        ]
+                    }
+                },
+                'total_placement': {
+                    '$sum': {
+                        '$multiply': [
+                            '$average_placement_at_stage', '$total_games'
+                        ]
+                    }
+                },
+                'total_games': {
+                    '$sum': '$total_games'
+                },
+                'wins': {
+                    '$sum': '$wins'
+                },
+                'placement_1_to_4': {
+                    '$sum': '$games_with_placement_1_to_4'
+                },
+                '_id': {
+                    'augment': '$_id.augment',
+                    'game_version': '$_id.game_version'
+                },
+                'average_placement_at_stage_2': {
+                    '$avg': {
+                        '$cond': [
+                            {
+                                '$eq': [
+                                    '$_id.augment_at_stage', 2
+                                ]
+                            }, '$average_placement_at_stage', None
                         ]
                     }
                 }
-            }, {
-                '$group': {
-                    '_id': {
-                        'augment': '$info.participants.augments',
-                        'augment_at_stage': '$augment_at_stage',
-                        'game_version': '$info.game_version'
-                    },
-                    'average_placement_at_stage': {
-                        '$avg': '$info.participants.placement'
-                    },
-                    'total_games': {
-                        '$sum': 1
-                    },
-                    'wins': {
-                        '$sum': {
-                            '$cond': [
-                                {
-                                    '$eq': [
-                                        '$info.participants.placement', 1
-                                    ]
-                                }, 1, 0
-                            ]
-                        }
-                    }
-                }
-            }, {
-                '$sort': {
-                    '_id.augment_at_stage': -1
-                }
-            }, {
-                '$out': 'augments_at_stage'
             }
-        ])
+        }, {
+            '$project': {
+                '_id': 0,
+                'augment': '$_id.augment',
+                'game_version': '$_id.game_version',
+                'average_placement_at_stage_3': 1,
+                'average_placement_at_stage_4': 1,
+                'win_percent': {
+                    '$multiply': [
+                        {
+                            '$divide': [
+                                '$wins', '$total_games'
+                            ]
+                        }, 100
+                    ]
+                },
+                'top4_percent': {
+                    '$multiply': [
+                        {
+                            '$divide': [
+                                '$placement_1_to_4', '$total_games'
+                            ]
+                        }, 100
+                    ]
+                },
+                'average_placement_at_stage_2': 1,
+                'average_placement': {
+                    '$divide': [
+                        '$total_placement', '$total_games'
+                    ]
+                },
+                'total_games': 1,
+                'wins': 1,
+                'placement_1_to_4': 1
+            }
+        }, {
+            '$out': 'augments'
+        }
+    ])
+    print("Done!")
 
 
 if __name__ == "__main__":
